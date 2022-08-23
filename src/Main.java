824bee4e68078d3044d5d6bdaf7f4203705e60b3
EmmanuelMutua <emmamulwa2020@gmail.com>
2022-08-23 23:00:10 +0300

    some changes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,35 @@
+class lamp{
+//creating a field
+    boolean isOn;
+
+    //method to turn on the light
+    void turnOn(){
+        isOn = true;
+        System.out.println("light is on" + isOn);
+    }
+
+    //method to turn off the light
+    void turnOff(){
+        isOn = false;
+        System.out.println("light is on?" + isOn);
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //creating an object of the lamp
+        lamp led = new lamp();
+        lamp halogen = new lamp();
+
+        //assess the method
+        System.out.println("Turning on the light");
+        led.turnOn();
+
+        System.out.println("Turning off the light");
+        halogen.turnOff();
+
+
+
     }
 }
